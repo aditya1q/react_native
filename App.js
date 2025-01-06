cdd281bc44b5ea5d8130636b08f5dee34acd624d
aditya1q <kanhatiwari9410@gmail.com>
2025-01-06 17:16:59 +0530

    first project

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import FlatCard from './components/project1/FlatCard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FlatCard />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView horizontal contentContainerStyle={styles.container}>
+        <FlatCard />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    // flexGrow: 1, // Ensure it takes up full height
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- components/project1/FlatCard.jsx
+++ components/project1/FlatCard.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 
 const FlatCard = () => {
-  return (
-    <View>
-      <Text>FlatCard</Text>
-    </View>
-  )
+    const Card = ({ bg }) => (
+        <View style={[styles.flatContainer, { backgroundColor: bg }]} />
+    )
+    return (
+        <View style={styles.container}>
+            <Card bg="#000" />
+            <Card bg="#534" />
+            <Card bg="#233" />
+        </View>
+    )
 }
+export default FlatCard;
 
-export default FlatCard
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        // flex: 1, // Use flex to fill the available space
+        flexDirection: 'row', // Distribute cards horizontally
+        // flexWrap: 'wrap', // Allow wrapping of items when space is constrained
+        // justifyContent: 'space-between', // Space between items
+        padding: 5, // Padding for the container
+        gap: 2
+    },
+    flatContainer: {
+        width: '100%', // Use percentage width for responsiveness
+        height: 100, // Fixed height for the cards
+        borderRadius: 10, // Rounded corners
+        // marginBottom: 10, // Space between cards vertically
+    }
+})
